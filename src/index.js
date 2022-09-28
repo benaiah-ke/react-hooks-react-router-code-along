9c5190cce3588f9d780ae83e04bdad7d92fd8ce3
benaiah-ke <benaiahpraise@gmail.com>
2022-09-28 08:18:20 +0300

    Done with lab

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
+import About from "./components/About";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import NavBar from "./components/Navbar";
 
-function Home() {
+function App() {
   return (
     <div>
-      <h1>Home!</h1>
+      <NavBar />
+
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+
+      </Routes>
     </div>
   );
 }
 
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
